7bc4de7c6cb837b539cc1cd70352755f676293e6
pauldjmetsi <pjnotts@yahoo.co.uk>
2022-01-17 16:00:33 +0000

    Added s3 bucket changes

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,10 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/metsi/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  credentials "app.terraform.io" {
-    # valid user API token:
-    token = "CyPd3zCWDGwgbw.atlasv1.3vlKjztXO96CvfMRk91xfsGhEGnAUwiUAUbBNmzrSfG6QYcC6mx9w1RugowzqFKm070"
-  }
 
   bucket = "my-s3-bucket"
   acl    = "private"
@@ -15,5 +11,5 @@ module "s3-bucket" {
   }
 
   bucket_prefix = var.prefix
-  
+
 }
